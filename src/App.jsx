265a3c02ff15e382a583eec95b761486b78596c2
Aditya Pandey <adityapandey2395@gmail.com>
2024-06-15 18:15:57 +0530

    Workflow Section Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import FeatureSection from "./components/FeatureSection"
 import HeroSection from "./components/HeroSection"
 import Navbar from "./components/Navbar"
+import Workflow from "./components/Workflow"
 
 function App() {
   return (
@@ -9,9 +10,8 @@ function App() {
       <div className="max-w-7xl mx-auto pt-20 px-6">
         <HeroSection />
         <FeatureSection />
+        <Workflow />
       </div>
-
-      
     </>
   )
 }
